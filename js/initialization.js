5e9aeade82aaabf4c709b13612b2234bd08bf798
QuachGiaHuy-web <quachhuygiaa2608@gmail.com>
2024-12-03 00:48:40 +0700

    Update initialization.js

--- js/initialization.js
+++ js/initialization.js
@@ -97,15 +97,6 @@ function createProduct() {
         {
             id: 11,
             status: 1, 
-            title: 'Bánh chuối nướng',
-            category: 'Món tráng miệng',
-            img: './assets/img/products/banh-chuoi-nuong.jpeg',
-            price: 60000,
-            desc: 'Bánh chuối nướng béo ngậy mùi nước cốt dừa cùng miếng chuối mềm ngon sẽ là Món tráng miệng phù hợp với mọi người.'
-        },
-        {
-            id: 12,
-            status: 1, 
             title: 'Bánh Kem Mousse Socola 2',
             img: './assets/img/products/socola2f.jpg',
             category: 'Bánh Kem Mousse',
@@ -113,7 +104,7 @@ function createProduct() {
             desc: 'Bánh Mousse Socola mang đến sự kết hợp hoàn hảo giữa vị đắng nhẹ của socola nguyên chất và lớp mousse mềm mịn. Vị ngọt ngào hòa quyện cùng chút béo ngậy tạo nên trải nghiệm tuyệt vời, khiến bạn khó cưỡng lại từ lần thử đầu tiên. Phù hợp cho mọi dịp, từ buổi tiệc sang trọng đến những khoảnh khắc thưởng thức nhẹ nhàng cùng gia đình.'
         },
         {
-            id: 13,
+            id: 12,
             status: 1, 
             title: 'Bánh Kem Mousse Socola Trái Cây',
             img: './assets/img/products/socolatraicay.jpg',
@@ -122,7 +113,7 @@ function createProduct() {
             desc: 'Bánh Mousse Socola mang đến sự kết hợp hoàn hảo giữa vị đắng nhẹ của socola nguyên chất và lớp mousse mềm mịn. Vị ngọt ngào hòa quyện cùng chút béo ngậy tạo nên trải nghiệm tuyệt vời, khiến bạn khó cưỡng lại từ lần thử đầu tiên. Phù hợp cho mọi dịp, từ buổi tiệc sang trọng đến những khoảnh khắc thưởng thức nhẹ nhàng cùng gia đình.'
         },
         {
-            id: 14,
+            id: 13,
             status: 1, 
             title: 'Bánh Mousse Bưởi Hồng',
             img: './assets/img/products/buoihong.jpg',
@@ -131,7 +122,7 @@ function createProduct() {
             desc: 'Bưởi hồng không chỉ mang lại cảm giác tươi mới mà còn giúp cân bằng vị giác, tạo nên một trải nghiệm ẩm thực vừa tinh tế vừa thú vị. Với mousse bưởi hồng, bạn sẽ có cảm giác như đang thưởng thức một món quà nhỏ bé nhưng đầy ý nghĩa trong mỗi lần cắt bánh.'
         },
         {
-            id: 15,
+            id: 14,
             status: 1, 
             title: 'Bánh Mousse Dưa Lưới',
             img: './assets/img/products/dualuoi.jpg',
@@ -140,7 +131,7 @@ function createProduct() {
             desc: 'Bánh mousse dưa lưới nổi bật với hương vị ngọt thanh của dưa lưới, hòa quyện với lớp mascarpone béo ngậy. Màu xanh dịu mát của dưa lưới không chỉ làm nổi bật vẻ ngoài tươi mới của bánh mà còn tạo cảm giác thư thái khi thưởng thức. Đặc biệt, mousse dưa lưới mang đến một trải nghiệm vị giác vừa tinh tế vừa sảng khoái, thích hợp cho cả người lớn và trẻ nhỏ.'
         },
         {
-            id: 16,
+            id: 15,
             status: 1, 
             title: 'Bánh Mousse Dưa Lưới Vàng',
             img: './assets/img/products/dualuoivang.jpg',
@@ -149,7 +140,7 @@ function createProduct() {
             desc: 'Mousse Dưa Lưới Vàng nổi bật với hương vị ngọt dịu và thanh mát từ những lát dưa lưới vàng tươi ngon. Lớp mousse mềm mịn kết hợp hoàn hảo với vị béo nhẹ, mang đến một trải nghiệm ẩm thực vừa tinh tế vừa đầy sảng khoái. Màu vàng ấm áp của dưa lưới không chỉ làm bánh trở nên bắt mắt mà còn tạo cảm giác thư giãn, dễ chịu khi thưởng thức.'
         },
         {
-            id: 17,
+            id: 16,
             status: 1, 
             title: 'Bánh Mousse Xoài',
             img: './assets/img/products/mousse xoài.jpg',
@@ -158,7 +149,7 @@ function createProduct() {
             desc: 'Với sắc vàng tươi tắn, mousse xoài không chỉ bắt mắt mà còn mang đến hương vị ngọt dịu, thanh mát từ những miếng xoài tươi chín mọng. Lớp mousse mềm mịn kết hợp cùng xoài tạo ra một sự hòa quyện hoàn hảo, mang lại cảm giác nhẹ nhàng và sảng khoái trong mỗi lần thưởng thức.'
         },
         {
-            id: 18,
+            id: 17,
             status: 1, 
             title: 'Bánh Mousse Bơ',
             img: './assets/img/products/mousse bơ.jpg',
@@ -167,7 +158,7 @@ function createProduct() {
             desc: 'Bơ tươi được chọn lọc kỹ lưỡng, mang đến một hương vị đậm đà và ngọt ngào mà không bị ngấy. Kết cấu mềm mịn của mousse bơ giúp bánh tan chảy ngay khi chạm vào đầu lưỡi, để lại dư vị dễ chịu và phong phú. Đây là một lựa chọn lý tưởng cho những buổi tiệc hay những dịp đặc biệt khi bạn muốn mang đến một món tráng miệng vừa ngon miệng vừa giàu dinh dưỡng.'
         },
         {
-            id: 19,
+            id: 18,
             status: 1, 
             title: 'Bánh Mousse Dâu Tây',
             img: './assets/img/products/mousse dâu tây.jpg',
@@ -176,7 +167,7 @@ function createProduct() {
             desc: 'Được làm từ những trái dâu tây tươi ngon nhất, món bánh này mang đến hương vị ngọt ngào, quyến rũ mà khó ai có thể chối từ. Sắc đỏ tươi tắn của dâu tây không chỉ thu hút ánh nhìn mà còn gợi lên cảm giác ấm áp và yêu thương.'
         },
         {
-            id: 20,
+            id: 19,
             status: 1, 
             title: 'Bánh Mousse Táo Xanh',
             img: './assets/img/products/mousse táo xanh.jpg',
@@ -185,7 +176,7 @@ function createProduct() {
             desc: 'Với lớp mousse mềm mịn kết hợp cùng táo xanh tươi mát, mỗi miếng bánh mang đến một cảm giác sảng khoái và thỏa mãn. Điểm đặc biệt của bánh là lớp trang trí bằng trái cây tươi như nho, dâu, và việt quất, không chỉ làm nổi bật vẻ đẹp mà còn tăng thêm sự phong phú về hương vị. Mousse táo xanh thực sự là một bản giao hưởng ngọt ngào dành cho những ai yêu thích trải nghiệm ẩm thực đa dạng.'
         },
         {
-            id: 21,
+            id: 20,
             status: 1, 
             title: 'Combo Bánh Ngọt (Cpmbo1)',
             category: 'Combo Bánh Ngọt',
@@ -194,7 +185,7 @@ function createProduct() {
             desc: 'Combo bánh ngọt'
         },
         {
-            id: 22,
+            id: 21,
             status: 1, 
             title: 'Combo Bánh Ngọt (Combo2)',
             category: 'Combo Bánh Ngọt',
@@ -204,7 +195,7 @@ function createProduct() {
         },
 
         {
-            id: 23,
+            id: 22,
             status: 1, 
             title: 'Combo Bánh Ngọt (Combo3)',
             category: 'Combo Bánh Ngọt',
@@ -214,7 +205,7 @@ function createProduct() {
         },
 
         {
-            id: 24,
+            id: 23,
             status: 1, 
             title: 'Combo Bánh Ngọt (Combo4)',
             img: './assets/img/products/cbn4.jpg',
@@ -223,7 +214,7 @@ function createProduct() {
             desc: 'Combo Bánh Ngọt'
         },
         {
-            id: 25,
+            id: 24,
             status: 1, 
             title: 'Combo Bánh Ngọt (Combo5)',
             img: './assets/img/products/cbn5.jpg',
@@ -233,7 +224,7 @@ function createProduct() {
         },
 
         {
-            id: 26,
+            id: 25,
             status: 1, 
             title: 'Combo Bánh Ngọt (Combo6)',
             category: 'Combo Bánh Ngọt',
@@ -243,7 +234,7 @@ function createProduct() {
         },
 
         {
-            id: 27,
+            id: 26,
             status: 1, 
             title: 'Combo Bánh Ngọt (Combo7)',
             category: 'Combo Bánh Ngọt',
@@ -253,7 +244,7 @@ function createProduct() {
         },
 
         {
-            id: 28,
+            id: 27,
             status: 1, 
             title: 'Combo Bánh Ngọt (Combo8)',
             category: 'Combo Bánh Ngọt',
@@ -263,7 +254,7 @@ function createProduct() {
         },
 
         {
-            id: 29,
+            id: 28,
             status: 1, 
             title: 'Combo Bánh Ngọt (Combo9)',
             category: 'Combo Bánh Ngọt',
@@ -273,7 +264,7 @@ function createProduct() {
         },
 
         {
-            id: 30,
+            id: 29,
             status: 1, 
             title: 'Combo Bánh Ngọt (Combo10)',
             category: 'Combo Bánh Ngọt',
@@ -283,7 +274,7 @@ function createProduct() {
         },
 
         {
-            id: 31,
+            id: 30,
             status: 1, 
             title: 'Nước Ép Dâu Tây',
             category: 'Nước uống',
@@ -291,18 +282,9 @@ function createProduct() {
             price: 350000,
             desc: 'được ép nguyên chất từ những quả dâu tây tươi'
         },
-        {
-            id: 32,
-            status: 1, 
-            title: 'Bánh bông lan chanh dây',
-            category: 'Món tráng miệng',
-            img: './assets/img/products/banh-bong-lan-chanh-day.jpeg',
-            price: 50000,
-            desc: 'Bánh bông lan chanh dây với vị chua nhẹ, không bị ngọt gắt hẳn sẽ là sự lựa chọn hoàn hảo'
-        },
 
         {
-            id: 33,
+            id: 31,
             status: 1, 
             title: 'Chè bưởi',
             category: 'Món tráng miệng',
